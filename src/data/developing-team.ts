cb0d9f22d5cdaf42a4da32546e3fd0000ef0c783
pradeepkadakol <pradeepkadakol144@gmail.com>
2026-09-26 09:58:26 +0630

    Fix developing

--- src/data/developing-team.ts
+++ src/data/developing-team.ts
@@ -8,26 +8,26 @@ export type DevelopingTeamMember = {
 export const DEVELOPING_TEAM: DevelopingTeamMember[] = [
   {
     id: "developer-1",
-    name: "Person 1",
+    name: "Manjunath P",
     role: "Developer",
-    image: "/images/developing-team/person1.jpg",
+    image: "/images/developing-team/person1.png",
   },
   {
     id: "developer-2",
     name: "Person 2",
     role: "Developer",
-    image: "/images/developing-team/person2.jpg",
+    image: "/images/developing-team/person2.png",
   },
   {
     id: "developer-3",
     name: "Person 3",
     role: "Developer",
-    image: "/images/developing-team/person3.jpg",
+    image: "/images/developing-team/person3.png",
   },
   {
     id: "developer-4",
     name: "Person 4",
     role: "Developer",
-    image: "/images/developing-team/person4.jpg",
+    image: "/images/developing-team/person4.png",
   },
 ];
